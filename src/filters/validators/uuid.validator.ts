766d7134e0380dda202acd1d169f4d7039d0da9a
Andrei Komau <andrei_komau@epam.com>
2024-11-11 00:44:54 +0100

    [dev] added validators and filters

---
+++ src/filters/validators/uuid.validator.ts
@@ -0,0 +1,13 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { isUUID } from 'class-validator';
+
+@Injectable()
+export class UUIDValidationPipe implements PipeTransform<string> {
+  transform(value: string): string {
+    if (!isUUID(value)) {
+      throw new BadRequestException(`Invalid UUID: ${value}`);
+    }
+
+    return value;
+  }
+}
