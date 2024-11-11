436b26cb19e758a2624ff1589ad05280d8fbca32
Andrei Komau <andrei_komau@epam.com>
2024-11-11 15:36:04 +0100

    [dev] added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { AlbumService } from './album/album.service';
+import { TrackService } from './track/track.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,10 +7,29 @@ import { UserModule } from './users/user.module';
 import { UserController } from './users/user.controller';
 import { UserService } from './users/user.service';
 import { DatabaseService } from './db/db';
+import { TrackModule } from './track/track.module';
+import { TrackController } from './track/track.controller';
+import { AlbumModule } from './album/album.module';
+import { AlbumController } from './album/album.controller';
+import { ArtistModule } from './artist/artist.module';
+import { ArtistController } from './artist/artist.controller';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService, DatabaseService],
+  imports: [UserModule, TrackModule, AlbumModule, ArtistController],
+  controllers: [
+    AppController,
+    UserController,
+    TrackController,
+    AlbumController,
+    ArtistModule,
+  ],
+  providers: [
+    AppService,
+    UserService,
+    TrackService,
+    AlbumService,
+    ArtistController,
+    DatabaseService,
+  ],
 })
 export class AppModule {}
