acb5b8f1e273f898c9dc8fbd3b45d1ccfb790e6e
Andrei Komau <andrei_komau@epam.com>
2024-11-11 12:59:01 +0100

    [dev] fixed the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
+import { UserController } from './users/user.controller';
+import { UserService } from './users/user.service';
+import { DatabaseService } from './db/db';
 
 @Module({
   imports: [UserModule],
-  // controllers: [AppController],
-  // providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService, DatabaseService],
 })
 export class AppModule {}
