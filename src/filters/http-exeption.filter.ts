f41bc8b2e956231fa70040fb288e5b1b2368e9f9
Andrei Komau <andrei_komau@epam.com>
2024-11-25 10:14:04 +0100

    [dev2-logger] added logger

---
+++ src/filters/http-exeption.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name, {
+    timestamp: true,
+  });
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+    const { method, url, query, body } = request;
+    const status = exception.getStatus() || HttpStatus.FORBIDDEN;
+    const exceptionResponse = exception.getResponse();
+
+    const message =
+      exceptionResponse['error'] || exception?.message || 'HTTP_EXCEPTION';
+
+    this.logger.warn(
+      `[FORBIDDEN]: "METHOD":${method} "URL":${url} "QUERY":${JSON.stringify(
+        query,
+      )} "BODY":${JSON.stringify(body)}`,
+    );
+
+    return response.status(status).json({
+      statusCode: status,
+      message,
+    });
+  }
+}
